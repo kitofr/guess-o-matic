615439dbd65d947310e58c9117ee95538e5ee809
Kristoffer Roupé <kitofr@gmail.com>
2015-10-22 18:56:07 +0200

    Add sounds update data

---
+++ sounds/get_sounds.rb
@@ -0,0 +1,8 @@
+
+('a'..'z').each do |ch|
+  `wget http://www.elevspel.se/amnen/svenska/bokstavsnamn/#{ch}.mp3`
+end
+
+`wget http://www.elevspel.se/amnen/svenska/bokstavsljud/aa.mp3`
+`wget http://www.elevspel.se/amnen/svenska/bokstavsljud/ae.mp3`
+`wget http://www.elevspel.se/amnen/svenska/bokstavsljud/oe.mp3`
--- src/Data.elm
+++ src/Data.elm
@@ -3,8 +3,11 @@ alternatives =
   [ { word = "APA", image = "http://www.skolbilder.com/Malarbild-apa-dm17524.jpg" }
   , { word = "MUS", image = "http://www.malarbok.nu/images/collection/169/large.jpg" }
   , { word = "KO", image = "http://ian.umces.edu/imagelibrary/albums/userpics/12789/normal_ian-symbol-bos-primigenius-cow-1.png" }
+  , { word = "BIL", image = "http://wallpaperhdbase.com/wp-content/gallery/car-cartoon-images/cartoon-car_104107391.jpg" }
   , { word = "HUS", image = "http://www.featurepics.com/FI/Thumb300/20110927/Cartoon-House-2009748.jpg" }
   , { word = "BUSS", image = "http://cdn.topvectors.com/img/605/600x0/4:3/cartoon-school-bus-vector-illustrator_1330497143_large.jpg" }
+  , { word = "PUSS", image = "http://www.agogowalls.in/wp-content/uploads/2015/09/funny-cartoon-kisses-first.jpg" }
+  , { word = "PRUTT", image = "http://www.cliparthut.com/clip-arts/784/cartoon-people-farting-784194.jpg" }
   , { word = "RÅTTA", image = "http://hdwallpaperslovely.com/wp-content/gallery/rat-cartoon-images/rats.jpg" }
   ]
 
