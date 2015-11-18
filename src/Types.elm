54c367e27445c9c67af8b3cf517223bb3eb8e07f
Kristoffer Roupé <kitofr@gmail.com>
2015-11-18 15:43:49 +0100

    Types

--- src/Types.elm
+++ src/Types.elm
@@ -21,12 +21,12 @@ type alias Model = {
 type alias CollectedChars = Set Char
 type alias Answer = String
 type alias Guess = (String, Answer)
-type alias WordList = List String
+type alias Question = { word: String, image: String }
+type alias WordList = List Question
 
 type GameState =
   FinishedGame CollectedChars 
   | Guessing Guess WordList CollectedChars
-
 
 wordList : GameState -> WordList
 wordList state =
@@ -40,19 +40,27 @@ collected state =
     (FinishedGame collected) -> collected
     (Guessing _ _ collected) -> collected
 
-initialState = (Guessing ("foo", "foo") ["foo", "bar"] Set.empty)
+initialState = (
+    Guessing ("foo", "foo") 
+    [{ word = "foo", image = "" }, { word = "bar", image = "" }] 
+    Set.empty)
 
-updateCollected set word = List.foldr (\c a-> Set.insert c a) set (String.toList word)
+updateCollected : Set Char -> Guess -> Set Char
+updateCollected set (guess, _) = List.foldr (\c a-> Set.insert c a) set (String.toList guess)
 
+correct : Guess -> Bool
 correct (guess, answer) = guess == answer
+
+word : Question -> String
+word question = question.word
 
 addGuess : Guess -> GameState -> GameState
 addGuess guess state = 
   let lst = wordList state
-      collected' = updateCollected (collected state) (fst guess)
+      collected' = updateCollected (collected state) guess
   in
     if | correct guess -> 
           case lst of 
             [] -> FinishedGame collected'
-            h::t -> Guessing ("", h) t collected'
+            h::t -> Guessing ("", (word h)) t collected'
        | otherwise -> Guessing guess lst (collected state)
