7211301a4b269b88f75a8d456674ec02c7f01dd1
Kristoffer Roupé <kitofr@gmail.com>
2015-12-01 14:51:19 +0100

    åäö

--- sounds/get_sounds.rb
+++ sounds/get_sounds.rb
@@ -1,7 +1,7 @@
 
-('a'..'z').each do |ch|
-  `wget http://www.elevspel.se/amnen/svenska/bokstavsnamn/#{ch}.mp3`
-end
+# ('a'..'z').each do |ch|
+#   `wget http://www.elevspel.se/amnen/svenska/bokstavsnamn/#{ch}.mp3`
+# end
 
 `wget http://www.elevspel.se/amnen/svenska/bokstavsljud/aa.mp3`
 `wget http://www.elevspel.se/amnen/svenska/bokstavsljud/ae.mp3`
