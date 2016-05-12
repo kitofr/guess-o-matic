fbd2e61aa12749e3f9e035defafc9997bfb32aed
Kristoffer Roupé <kitofr@gmail.com>
2016-05-12 17:48:12 +0200

    Add back words

--- src/Data.elm
+++ src/Data.elm
@@ -6,37 +6,37 @@ alternatives =
   [ { word = "IS", image = "http://4vector.com/i/free-vector-ice-cube-clip-art_113098_Ice_Cube_clip_art_medium.png" }
   , { word = "KO", image = "http://ian.umces.edu/imagelibrary/albums/userpics/12789/normal_ian-symbol-bos-primigenius-cow-1.png" }
   , { word = "APA", image = "http://www.skolbilder.com/Malarbild-apa-dm17524.jpg" }
---  , { word = "MUS", image = "http://www.malarbok.nu/images/collection/169/large.jpg" }
---  , { word = "BIL", image = "http://wallpaperhdbase.com/wp-content/gallery/car-cartoon-images/cartoon-car_104107391.jpg" }
---  , { word = "ELD", image = "http://4vector.com/i/free-vector-fire-icon-clip-art_117494_Fire_Icon_clip_art_medium.png" }
---  , { word = "YXA", image = "http://pre05.deviantart.net/70f7/th/pre/i/2011/063/3/7/axe_sketch_by_shanebrake-d3awhqm.jpg" }
---  , { word = "DANS", image = "http://4vector.com/i/free-vector-dancer-clip-art_111608_Dancer_clip_art_medium.png" }
---  , { word = "BOK", image = "http://4vector.com/i/free-vector-open-book-clip-art_111581_Open_Book_clip_art_medium.png" }
---  , { word = "HUS", image = "http://www.featurepics.com/FI/Thumb300/20110927/Cartoon-House-2009748.jpg" }
---  , { word = "ORM", image = "http://4vector.com/data/127/989/thumb/127989_1_s.jpg" }
---  , { word = "VAL", image = "http://4vector.com/i/free-vector-small-whale-clip-art_110039_Small_Whale_clip_art_medium.png" }
---  , { word = "HUND", image = "http://4vector.com/i/free-vector-cartoon-dog-clip-art_128453_cartoon-dog-clip-art/Cartoon_Dog_clip_art_hight.png" }
---  , { word = "LÄSA", image = "http://4vector.com/i/free-vector-person-reading-book-clip-art_110276_Person_Reading_Book_clip_art_medium.png" }
---  , { word = "KATT", image = "http://4vector.com/i/free-vector-cartoon-cat-sitting-clip-art_118984_Cartoon_Cat_Sitting_clip_art_medium.png" }
---  , { word = "BUSS", image = "http://cdn.topvectors.com/img/605/600x0/4:3/cartoon-school-bus-vector-illustrator_1330497143_large.jpg" }
---  , { word = "NÄSA", image = "http://4vector.com/i/free-vector-big-nosed-man_102324_Big_nosed_man.png" }
---  , { word = "ÄLVA", image = "http://4vector.com/i/free-vector-flower-fairy-02-vector_020726_2.jpg" }
---  , { word = "PUSS", image = "http://www.agogowalls.in/wp-content/uploads/2015/09/funny-cartoon-kisses-first.jpg" }
---  , { word = "ÖDLA", image = "http://4vector.com/i/free-vector-az-lizard-clip-art_127966_az-lizard-clip-art/Azlizard_clip_art_medium.png" }
---  , { word = "ZEBRA", image = "http://4vector.com/i/free-vector-studiofibonacci-cartoon-zebra-clip-art_118409_Studiofibonacci_Cartoon_Zebra_clip_art_medium.png" }
---  , { word = "TROLL", image = "https://s-media-cache-ak0.pinimg.com/236x/f4/7c/95/f47c95d14766c0be11c12425441688b3.jpg" }
---  , { word = "REGN", image = "http://4vector.com/i/free-vector-rain-cloud-clip-art_109955_Rain_Cloud_clip_art_medium.png" }
---  , { word = "PENGAR", image = "http://4vector.com/i/free-vector-money-bag-clip-art_106168_Money_Bag_clip_art_medium.png" }
---  , { word = "ÅSNA", image = "http://4vector.com/data/129/801/thumb/129801_1_s.jpg" }
---  , { word = "GLASS", image = "http://4vector.com/i/free-vector-soft-ice-cream-cones-ff-menu-clip-art_120791_soft-ice-cream-cones-ff-menu-clip-art/Soft_Ice_Cream_Cones_Ff_Menu_clip_art_medium.png" }
---  , { word = "MAMMA", image = "http://4vector.com/data/125/051/thumb/125051_1_s.jpg" }
---  , { word = "FAMILJ", image = "http://4vector.com/data/125/483/thumb/125483_1_s.jpg" }
---  , { word = "PRUTT", image = "http://www.cliparthut.com/clip-arts/784/cartoon-people-farting-784194.jpg" }
---  , { word = "RÅTTA", image = "http://hdwallpaperslovely.com/wp-content/gallery/rat-cartoon-images/rats.jpg" }
---  , { word = "SPINDEL", image = "http://4vector.com/i/free-vector-black-widow-spider_101432_Black_Widow_Spider.png" }
---  , { word = "ELEFANT", image = "http://4vector.com/i/free-vector-cartoon-elephant-clip-art_106801_Cartoon_Elephant_clip_art_medium.png" }
---  , { word = "CIRKUS", image = "http://4vector.com/i/free-vector-circus-tent-clip-art_110158_Circus_Tent_clip_art_medium.png" }
---  , { word = "JORDGUBBE", image = "http://4vector.com/i/free-vector-outline-strawberry-clip-art_112728_Outline_Strawberry_clip_art_medium.png" }
+  , { word = "MUS", image = "http://www.malarbok.nu/images/collection/169/large.jpg" }
+  , { word = "BIL", image = "http://wallpaperhdbase.com/wp-content/gallery/car-cartoon-images/cartoon-car_104107391.jpg" }
+  , { word = "ELD", image = "http://4vector.com/i/free-vector-fire-icon-clip-art_117494_Fire_Icon_clip_art_medium.png" }
+  , { word = "YXA", image = "http://pre05.deviantart.net/70f7/th/pre/i/2011/063/3/7/axe_sketch_by_shanebrake-d3awhqm.jpg" }
+  , { word = "DANS", image = "http://4vector.com/i/free-vector-dancer-clip-art_111608_Dancer_clip_art_medium.png" }
+  , { word = "BOK", image = "http://4vector.com/i/free-vector-open-book-clip-art_111581_Open_Book_clip_art_medium.png" }
+  , { word = "HUS", image = "http://www.featurepics.com/FI/Thumb300/20110927/Cartoon-House-2009748.jpg" }
+  , { word = "ORM", image = "http://4vector.com/data/127/989/thumb/127989_1_s.jpg" }
+  , { word = "VAL", image = "http://4vector.com/i/free-vector-small-whale-clip-art_110039_Small_Whale_clip_art_medium.png" }
+  , { word = "HUND", image = "http://4vector.com/i/free-vector-cartoon-dog-clip-art_128453_cartoon-dog-clip-art/Cartoon_Dog_clip_art_hight.png" }
+  , { word = "LÄSA", image = "http://4vector.com/i/free-vector-person-reading-book-clip-art_110276_Person_Reading_Book_clip_art_medium.png" }
+  , { word = "KATT", image = "http://4vector.com/i/free-vector-cartoon-cat-sitting-clip-art_118984_Cartoon_Cat_Sitting_clip_art_medium.png" }
+  , { word = "BUSS", image = "http://cdn.topvectors.com/img/605/600x0/4:3/cartoon-school-bus-vector-illustrator_1330497143_large.jpg" }
+  , { word = "NÄSA", image = "http://4vector.com/i/free-vector-big-nosed-man_102324_Big_nosed_man.png" }
+  , { word = "ÄLVA", image = "http://4vector.com/i/free-vector-flower-fairy-02-vector_020726_2.jpg" }
+  , { word = "PUSS", image = "http://www.agogowalls.in/wp-content/uploads/2015/09/funny-cartoon-kisses-first.jpg" }
+  , { word = "ÖDLA", image = "http://4vector.com/i/free-vector-az-lizard-clip-art_127966_az-lizard-clip-art/Azlizard_clip_art_medium.png" }
+  , { word = "ZEBRA", image = "http://4vector.com/i/free-vector-studiofibonacci-cartoon-zebra-clip-art_118409_Studiofibonacci_Cartoon_Zebra_clip_art_medium.png" }
+  , { word = "TROLL", image = "https://s-media-cache-ak0.pinimg.com/236x/f4/7c/95/f47c95d14766c0be11c12425441688b3.jpg" }
+  , { word = "REGN", image = "http://4vector.com/i/free-vector-rain-cloud-clip-art_109955_Rain_Cloud_clip_art_medium.png" }
+  , { word = "PENGAR", image = "http://4vector.com/i/free-vector-money-bag-clip-art_106168_Money_Bag_clip_art_medium.png" }
+  , { word = "ÅSNA", image = "http://4vector.com/data/129/801/thumb/129801_1_s.jpg" }
+  , { word = "GLASS", image = "http://4vector.com/i/free-vector-soft-ice-cream-cones-ff-menu-clip-art_120791_soft-ice-cream-cones-ff-menu-clip-art/Soft_Ice_Cream_Cones_Ff_Menu_clip_art_medium.png" }
+  , { word = "MAMMA", image = "http://4vector.com/data/125/051/thumb/125051_1_s.jpg" }
+  , { word = "FAMILJ", image = "http://4vector.com/data/125/483/thumb/125483_1_s.jpg" }
+  , { word = "PRUTT", image = "http://www.cliparthut.com/clip-arts/784/cartoon-people-farting-784194.jpg" }
+  , { word = "RÅTTA", image = "http://hdwallpaperslovely.com/wp-content/gallery/rat-cartoon-images/rats.jpg" }
+  , { word = "SPINDEL", image = "http://4vector.com/i/free-vector-black-widow-spider_101432_Black_Widow_Spider.png" }
+  , { word = "ELEFANT", image = "http://4vector.com/i/free-vector-cartoon-elephant-clip-art_106801_Cartoon_Elephant_clip_art_medium.png" }
+  , { word = "CIRKUS", image = "http://4vector.com/i/free-vector-circus-tent-clip-art_110158_Circus_Tent_clip_art_medium.png" }
+  , { word = "JORDGUBBE", image = "http://4vector.com/i/free-vector-outline-strawberry-clip-art_112728_Outline_Strawberry_clip_art_medium.png" }
   ]
 
 
