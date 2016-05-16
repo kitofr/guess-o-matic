b6eb9cc6be88dbaa17b4a8410b4d4869cbb2727e
Kristoffer Roupé <kitofr@gmail.com>
2016-05-16 23:17:03 +0200

    WIP: Restyle

--- src/View.elm
+++ src/View.elm
@@ -5,6 +5,7 @@ import Html.Attributes as A
 import Html.Events exposing (onClick)
 import String exposing (..)
 import Set exposing (..)
+import List exposing (append)
 import Types exposing (..)
 import Seq exposing (..)
 import Data exposing (..)
@@ -12,10 +13,33 @@ import Svg
 import Svg.Attributes as SvgA
 
 row : List (Html Msg) -> Html Msg
-row = div [ A.class "row", rowDistance ]
+row = div [ A.class "row", rowDistance]
+
+rowDistance =
+  A.style [ ( "margin-bottom","10px"), ("margin-top", "25px") ]
+
+questionArea =
+  div [ A.style [ ("width", "90%")
+                 ,("display", "block")
+                 ,("border-radius", "10px")
+                 ,("background", "white")
+                 ,("padding", "10px")
+                 ,("margin", "auto")]]
 
 container_ : List (Html Msg) -> Html Msg
-container_ = div [ A.class "container-fluid"]
+container_ = div [ A.class "container-fluid"
+                  ,A.style [("background-color", "#EEE")
+                           ,("width", "100%")]]
+
+centerInline width =
+  A.style [("width", (toString width) ++ "%")
+          ,("display", "inline-block")
+          ,("margin", "auto")]
+
+center width =
+  [("width", (toString width) ++ "%")
+  ,("display", "block")
+  ,("margin", "auto")]
 
 stylesheet : String -> Html Msg
 stylesheet href =
@@ -27,6 +51,16 @@ stylesheet href =
 smallButtonStyle =
   A.style [("font-size", "16px")
           ,("margin-right", "5px")]
+
+pushToBottom =
+  [A.style [ ("margin-top", "100px")
+           , ("margin-bottom", "100px")]]
+
+checkButton =
+  let style = List.append (center 90) [("font-size", "35px")]
+  in
+  [ A.style style
+  , A.class "btn btn-success"]
 
 megaButton =
   A.style [
@@ -40,9 +74,6 @@ buttonStyle =
   ,("width", "100px")
   ,("font-size", "20px")
   ,("padding", "10px")]
-
-rowDistance =
-  A.style [ ( "margin-bottom","10px"), ("margin-top", "25px") ]
 
 btn action classes what =
   button [ classes, buttonStyle ,onClick action ]
@@ -171,25 +202,31 @@ progress : Model -> Html Msg
 progress {guess, state} =
   let n = wordList state |> List.length |> Basics.toFloat
       tot = alternatives |> List.length |> Basics.toFloat
-      total = 300.0
+      total = 500.0
       complete = ((tot-n) / tot) * total
       width = (toString total)
   in
-      Svg.svg
-        [ SvgA.width width, SvgA.height "20", SvgA.viewBox ("0 0 " ++ width ++ " 20") ]
-        [ Svg.rect [ SvgA.fill "#AAA", SvgA.x "0", SvgA.y "0", SvgA.width width, SvgA.height "20", SvgA.rx "5", SvgA.ry "5" ] [],
-          Svg.rect [ SvgA.fill "#8C8", SvgA.x "0", SvgA.y "0", SvgA.width (toString complete), SvgA.height "20", SvgA.rx "5", SvgA.ry "5" ] []
-        ]
+    div [centerInline 90]
+        [Svg.svg
+           [ SvgA.width "100%", SvgA.height "20", SvgA.viewBox ("0 0 " ++ width ++ " 20") ]
+           [ Svg.rect [ SvgA.fill "#AAA", SvgA.x "0", SvgA.y "0", SvgA.width width, SvgA.height "20", SvgA.rx "5", SvgA.ry "5" ] [],
+               Svg.rect [ SvgA.fill "#8C8", SvgA.x "0", SvgA.y "0", SvgA.width (toString complete), SvgA.height "20", SvgA.rx "5", SvgA.ry "5" ] []
+        ]]
+
+quit =
+  span [A.style [("float", "left")
+                ,("margin-left", "20px")]]
+      [ button [A.style [("display", "inline")]] [text "Quit"]]
 
 score : Model -> Html Msg
 score {guess, state} =
    let score = (currentScore state)
        fontX = (toString (85 - ((score // 10) * 12)))
    in
-        Svg.svg
-            [ SvgA.width "200", SvgA.height "200", SvgA.viewBox "0 0 200 200"]
+     Svg.svg
+          [ SvgA.width "200", SvgA.height "200", SvgA.viewBox "0 0 200 200"]
             [ Svg.polygon [ SvgA.fill "#EE9"
-            , SvgA.points "100,10 40,198 190,78 10,78 160,198" ] []
+                          , SvgA.points "100,10 40,198 190,78 10,78 160,198" ] []
             , Svg.text'
                 [SvgA.fontSize "45", SvgA.x fontX, SvgA.y "130", SvgA.fill "blue"]
                 [Svg.text (toString score)] ]
@@ -202,15 +239,22 @@ view model =
     , body []
        [container_
           [ row
-              [ picture model
-              , div [A.class "row"]
-                  [score model
-                  , success model]]
-              , progress model
-          , textControls model
-          , showGuess model
-          , letterButtons model
-          , soundButtons model
+              [ quit
+              , progress model]
+          , row
+              [questionArea
+                 [ picture model
+                 , div [A.class "row"]
+                 [--score model
+                    success model]
+
+                 , showGuess model
+                 , letterButtons model
+                 , soundButtons model]
+                 ]
+              --, textControls model
+          , div pushToBottom
+                [button checkButton [text "Check"]]
        ]
       ]
     ]
